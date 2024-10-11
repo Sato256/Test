29cd880eaea105a0c06edabdcd72fa0b065c3d78
OGAWA11 <ogwshm1@gmail.com>
2024-10-11 19:47:24 +0900

    oga

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "oga",
+                        name = "ogachan",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TestTheme {
-        Greeting("oga")
+        Greeting("ogachan")
     }
 }
